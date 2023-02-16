a28994f7c164087fa0ed3d9a1b86aa01136ceb59
Inbalokesh <116268697+Inbalokesh@users.noreply.github.com>
2023-02-17 00:03:40 +0530

    Completed patterns

---
+++ DSA/array hw .js
@@ -0,0 +1,57 @@
+// 1. Given 2 numbers it should print if they are equal or not equal,
+// let a = 10;
+// let b = 20;
+
+// let a = 10;
+// let b = 10;
+// if(a!==b){
+//     console.log("a is not equal to b")
+// }
+// else{
+//     console.log("a is equal to b")
+// }
+
+// 2. For a given positive integer n, it should print every number from 1 to n except x
+
+// let n=10;
+// let x=10;
+// let num="";
+// for(let i=1; i<=n; i++){
+//     if(i!==x){
+//         num+=i+" ";
+//     }   
+// }
+// console.log(num)
+
+
+//3.  Given an array, print the sum of all even numbers in the array
+
+// a = [1, 2, 3, 4, 5, 6, 8];
+// x = a.length;
+// num = 0;
+// for (let i = 0; i < x; i++) {
+//     if (a[i] % 2 == 0) {
+//         num += a[i];
+//     }
+// }
+// console.log(num)
+
+// 4. Given an array of length n and k (k < n) find the maximum sum of k consecutive numbers.
+// for eg:
+// let a = [4,5, 10, 11, 1];
+// let k = 3;
+
+// a = [4, 5, 10, 11, 1];
+// let k = 3;
+// let max = 0;
+// for (let i = 0; i < a.length - k + 1; i++) {
+//     let num = 0;
+//     for (let j = i; j < i + k; j++) {
+//         num += a[j];
+//     }
+//     if (num > max) {
+//         max = num;
+//     }
+// }
+// console.log(max)
+
